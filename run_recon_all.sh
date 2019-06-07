c547d8129bfecf696db154b399045ca8db893ba6
Tommy Sprague <PSYCH-ADS\sprague@tcs-compute-1.psych.ucsb.edu>
2019-06-07 11:10:11 -0700

    updates to run_recon_all

--- run_recon_all.sh
+++ run_recon_all.sh
@@ -3,20 +3,46 @@
 
 SUBJ=$1
 
-cd $SUBJECTS_DIR
+# if 2 or more arguments, subjects_dir is the second one; otherwise, use the freesurfer one...
+if [ $# -ge 2 ]
+then
+    SUBJ_DIR=$2
+else
+    SUBJ_DIR=$SUBJECTS_DIR   # otherwise, get the freesurfer value
+fi
 
-recon-all -autorecon1 -autorecon2 -autorecon3 -sd $SUBJECTS_DIR -subjid ${SUBJ}anat \
-    -i $SUBJECTS_DIR/${SUBJ}src/T1_1.nii* \
-    -i $SUBJECTS_DIR/${SUBJ}src/T1_2.nii* \
-    -i $SUBJECTS_DIR/${SUBJ}src/T1_3.nii* \
+
+
+# if 3 arguments, use 3rd as # of 
+if [ $# -ge 3 ]
+then
+    OPENMP_CORES=$3
+else
+    OPENMP_CORES=30
+fi
+
+
+#cd $SUBJECTS_DIR
+
+# recon-all -autorecon1 -autorecon2 -autorecon3 -sd $SUBJECTS_DIR -subjid ${SUBJ}anat \
+#     -i $SUBJECTS_DIR/${SUBJ}src/T1_1.nii* \
+#     -i $SUBJECTS_DIR/${SUBJ}src/T1_2.nii* \
+#     -i $SUBJECTS_DIR/${SUBJ}src/T1_3.nii* \
+#     -expert $PREPROC/hires_expert \
+#     -parallel -openmp 16 -hires
+
+
+# recon-all -autorecon1 -autorecon2 -autorecon3 -sd $SUBJ_DIR -subjid ${SUBJ}anat \
+#     -i  $SUBJ_DIR/${SUBJ}src/T1_1.nii* \
+#     -i  $SUBJ_DIR/${SUBJ}src/T1_2.nii* \
+#     -i  $SUBJ_DIR/${SUBJ}src/T1_3.nii* \
+#     -T2 $SUBJ_DIR/${SUBJ}src/T2_1.nii* \
+#     -expert $PREPROC/hires_expert \
+#     -T2pial -parallel -openmp $OPENMP_CORES -hires
+
+
+recon-all -autorecon1 -autorecon2 -autorecon3 -sd $SUBJ_DIR -subjid ${SUBJ}anat \
+    -i  $SUBJ_DIR/${SUBJ}src/T1_1.nii* \
+    -T2 $SUBJ_DIR/${SUBJ}src/T2_1.nii* \
     -expert $PREPROC/hires_expert \
-    -parallel -openmp 16 -hires
-
-
-    # recon-all -autorecon1 -autorecon2 -autorecon3 -sd $SUBJECTS_DIR -subjid ${SUBJ}anat \
-    #     -i $SUBJECTS_DIR/${SUBJ}src/T1_1.nii* \
-    #     -i $SUBJECTS_DIR/${SUBJ}src/T1_2.nii* \
-    #     -i $SUBJECTS_DIR/${SUBJ}src/T1_3.nii* \
-    #     -T2 $SUBJECTS_DIR/${SUBJ}src/T2_1.nii* \
-    #     -expert $PREPROC/hires_expert \
-    #     -T2pial -parallel -openmp 16 -hires
+    -T2pial -parallel -openmp $OPENMP_CORES -hires
